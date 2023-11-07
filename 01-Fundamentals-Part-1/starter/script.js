a5b4b10203c24c09acc275b2ba9f23e318c115df
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2023-11-07 16:24:54 +0100

    14

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,50 @@
+// let js = "amazing";
+// if (js === "amazing") alert("JS is fun");
+// console.log(2 + 2);
+
+// console.log("jeremy")
+
+// console.log("22")
+
+// let 3years = 3
+
+let javascriptIsFun = true
+console.log(typeof true)
+console.log(typeof javascriptIsFun)
+console.log(typeof 23)
+console.log(typeof 'Jeremy')
+
+
+// javascriptIsFun = 'oui'
+
+// let year;
+// year = 1990;
+
+// console.log(typeof null);
+
+// let age = 32;
+// age = 33;
+
+// const birthYear = 1990;
+
+// birthYear = 1991
+// console.log(birthYear)
+
+const firstName = "jeremy"
+const lastName = "Vangansberg"
+
+console.log(firstName + ' ' +lastName)
+
+let x = 2 + 2
+console.log(x)
+
+x += 10
+x++; 
+x++; 
+console.log(x)
+
+const ageJeremy = 33;
+console.log(ageJeremy > 40)
+
+const isFullAge = ageJeremy >= 18;
+console.log(isFullAge)
